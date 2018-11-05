c82f1a7dbb0e280edb8e6f48d8d7666204657531
razkosser <razkosser@gmail.com>
2018-11-05 16:47:14 +0700

    Готово

---
+++ HW/script/script.js
@@ -0,0 +1,11 @@
+let temp = Number(prompt("Введите вашу температуру"));
+alert("Такова ваша температура по фаренгейту " + (Tf = (9 / 5) * temp + 3));
+
+
+let	name = "Василий";
+let admin = name;
+alert(admin);
+
+
+let what = 1000 + "108";
+alert("Будет равно " + what);
